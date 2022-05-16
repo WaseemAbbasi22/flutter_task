c7ac47f16181fed5c1fe4c2bdec8d254ca1d8106
waseem_abbasi-at-828518973552 <waseem_abbasi-at-828518973552>
2022-05-16 14:24:16 +0500

    "profile page changes done"

---
+++ lib/data/extentions.dart
@@ -0,0 +1,27 @@
+extension extString on String {
+
+
+  bool get isValidName{
+    final nameRegExp = new RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$");
+    return nameRegExp.hasMatch(this);
+  }
+
+  bool get isValidTitle{
+    final nameRegExp = new RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$");
+    return nameRegExp.hasMatch(this);
+  }
+
+  bool get isNotNull{
+    return this!=null;
+  }
+
+  bool get isValidIncome{
+    final phoneRegExp = RegExp(r"^\+?0[0-9]{10}$");
+    return phoneRegExp.hasMatch(this);
+  }
+  // bool get isValidPicture{
+  //   final phoneRegExp = RegExp(r"^\+?0[0-9]{10}$");
+  //   return phoneRegExp.hasMatch(this);
+  // }
+
+}
---
+++ lib/views/Forms/form_validation_vm.dart
@@ -0,0 +1,62 @@
+import 'package:rns_flutter_task/data/base/base_vm.dart';
+import 'package:rns_flutter_task/data/extentions.dart';
+import 'package:rns_flutter_task/data/models/validation_model.dart';
+
+class FormValidationVm extends BaseVm{
+  ValidationModel _firstName = ValidationModel(null, null);
+
+  ValidationModel get firstName => _firstName;
+
+  set firstName(ValidationModel value) {
+    _firstName = value;
+  }
+
+  ValidationModel _lastName = ValidationModel(null, null);
+  ValidationModel _monthlyIncome = ValidationModel(null, null);
+  ValidationModel _invoicePicture = ValidationModel(null, null);
+  ValidationModel _jobTitle = ValidationModel(null, null);
+
+  ValidationModel get lastName => _lastName;
+
+  set lastName(ValidationModel value) {
+    _lastName = value;
+    notifyListeners();
+  }
+
+  ValidationModel get monthlyIncome => _monthlyIncome;
+
+  set monthlyIncome(ValidationModel value) {
+    _monthlyIncome = value;
+    notifyListeners();
+  }
+
+  ValidationModel get invoicePicture => _invoicePicture;
+
+  set invoicePicture(ValidationModel value) {
+    _invoicePicture = value;
+    notifyListeners();
+  }
+
+  ValidationModel get jobTitle => _jobTitle;
+
+  set jobTitle(ValidationModel value) {
+    _jobTitle = value;
+    notifyListeners();
+  }
+  void validateFirstName(String? val) {
+    if (val != null && val.isValidName) {
+      firstName = ValidationModel(val, null);
+    } else {
+      firstName = ValidationModel(null, 'Please Enter a Valid Name');
+    }
+    notifyListeners();
+  }
+  void validateLastName(String? val) {
+    if (val != null && val.isValidName) {
+      lastName = ValidationModel(val, null);
+    } else {
+      lastName = ValidationModel(null, 'Please Enter a Valid Name');
+    }
+    notifyListeners();
+  }
+}
